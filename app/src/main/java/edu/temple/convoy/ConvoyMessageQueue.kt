4fe258b91f72f847ccbea343c8292ac898f169e3
David Fiel <github@dfiel.com>
2023-03-11 18:38:08 -0500

    Make Queue concrete class instead of extending queue

--- app/src/main/java/edu/temple/convoy/ConvoyMessageQueue.kt
+++ app/src/main/java/edu/temple/convoy/ConvoyMessageQueue.kt
@@ -11,7 +11,7 @@ import java.io.File
 import java.net.URL
 import java.util.concurrent.ConcurrentLinkedQueue
 
-class ConvoyMessageQueue(val context: Context) : ConcurrentLinkedQueue<ConvoyMessageQueue.ConvoyMessage>() {
+class ConvoyMessageQueue(private val context: Context, private val scope: CoroutineScope) {
     data class ConvoyMessage(val username: String, val file: File)
 
     private var mediaPlayer = MediaPlayer().apply {
@@ -21,36 +21,45 @@ class ConvoyMessageQueue(val context: Context) : ConcurrentLinkedQueue<ConvoyMes
                 .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
                 .build()
         )
-        setOnCompletionListener { reset(); queueMessage() }
+        setOnCompletionListener { cleanupMessage(); queueMessage() }
         setOnPreparedListener { start() }
     }
+    private var queue = ConcurrentLinkedQueue<ConvoyMessage>()
 
-    fun downloadMessage(scope: CoroutineScope, baseDir: File, messageURL: URL, username: String) {
+    fun add(element: ConvoyMessage?) {
+        queue.add(element)
+        if (!mediaPlayer.isPlaying) {
+            queueMessage()
+        }
+    }
+
+    fun downloadMessage(messageURL: URL, username: String) {
         scope.launch(Dispatchers.IO) {
             val recordingFile = File(
-                baseDir.absolutePath + File.separator + username +
-                        '-' + System.nanoTime() + ".rec.m4a"
+                context.filesDir.absolutePath + File.separator + username + '-' +
+                        System.nanoTime() + ".rec.m4a"
             )
             recordingFile.writeBytes(messageURL.readBytes())
             this@ConvoyMessageQueue.add(ConvoyMessage(username, recordingFile))
         }
     }
 
-    private fun queueMessage() {
-        if (this.size == 0) { return }
-        val message = this.poll()
-        mediaPlayer.apply {
-            setDataSource(context, Uri.fromFile(message!!.file))
-            setVolume(1f,1f)
-            prepareAsync()
+    private fun cleanupMessage() {
+        val message = queue.poll()
+        scope.launch(Dispatchers.IO) {
+            message!!.file.delete()
         }
     }
 
-    override fun add(element: ConvoyMessage?): Boolean {
-        super.add(element)
-        if (!mediaPlayer.isPlaying) {
-            queueMessage()
+    private fun queueMessage() {
+        mediaPlayer.reset()
+        if (queue.size == 0) {
+            return
         }
-        return true
+        val message = queue.peek()
+        mediaPlayer.apply {
+            setDataSource(context, Uri.fromFile(message!!.file))
+            prepareAsync()
+        }
     }
 }
